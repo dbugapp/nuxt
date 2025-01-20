75fcb2df003f7a774259aa9dfa039cd38b0408b6
kevin olson <acidjazz@gmail.com>
2025-01-19 18:32:52 -0600

    🚧 lets see if we dont include ModuleOptions

--- src/runtime/nitro.ts
+++ src/runtime/nitro.ts
@@ -1,10 +1,9 @@
 import { defineNitroPlugin, useRuntimeConfig } from 'nitropack/runtime'
-import type { ModuleOptions } from '../../../module'
-import { careNitroError } from '../care'
+import { careNitroError } from './care'
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook('error', async (error, { event }) => {
-    const config = useRuntimeConfig(event).public.care as Required<ModuleOptions>
+    const config = useRuntimeConfig(event).public.care as Required<{ apiKey: string, apiDomain?: string }>
     careNitroError(error, config)
   })
 })
--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,9 +1,8 @@
-import type { ModuleOptions } from '../module'
 import { careVueError, careAppError } from './care'
 import { defineNuxtPlugin, useRuntimeConfig } from '#app'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig().public.care as Required<ModuleOptions>
+  const config = useRuntimeConfig().public.care as Required<{ apiKey: string, apiDomain?: string }>
   nuxtApp.hook('vue:error', (error: unknown, _instance, _info) => careVueError(error, config))
   nuxtApp.hook('app:error', (error: unknown) => careAppError(error, config))
 })
